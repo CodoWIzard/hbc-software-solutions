56b262081b52fdbd03309a61f4c7741a9bea22fb
Jayen <jayden.budike@gmail.com>
2025-02-21 10:11:49 +0100

    added php files and made an entry point for index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,26 @@
 <?php
+require './config/database.php';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+
+if ($uri[0] === 'products') {
+    require './src/routes/products.php';
+} elseif ($uri[0] === 'orders') {
+    require './src/routes/orders.php';
+} else {
+    http_response_code(404);
+    echo json_encode(["message" => "Route not found"]);
+}
+
+?>
+
+
+
+
+
+<?php
 include 'header.php';
 ?>
 
